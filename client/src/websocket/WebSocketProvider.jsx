a117b6252fdc0ee31d01ea42bc2e92beb80922a7
Hoang105205 <155875858+Hoang105205@users.noreply.github.com>
2025-06-25 11:00:23 +0700

    fix zuztand storage & initial websocket

---
+++ client/src/websocket/WebSocketProvider.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useEffect } from 'react';
+import connectWebSocket from './websocket-client';
+
+// Tạo Context
+export const WebSocketContext = createContext(null);
+
+// Tạo Provider để quản lý WebSocket
+export const WebSocketProvider = ({ children }) => {
+  const [ws, setWebSocket] = useState(null);
+
+  useEffect(() => {
+    // Kết nối WebSocket khi ứng dụng khởi động
+    const websocket = connectWebSocket();
+    setWebSocket(websocket);
+
+    // Cleanup: Ngắt kết nối WebSocket khi component bị hủy
+    return () => {
+      websocket.close();
+    };
+  }, []);
+
+  return (
+    <WebSocketContext.Provider value={ws}>
+      {children}
+    </WebSocketContext.Provider>
+  );
+};
