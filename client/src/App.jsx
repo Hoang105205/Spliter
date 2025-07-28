33f8d0efbb1b1b3c10d8cbdf374c05b71b334431
Hoang105205 <huyhoangluu105@gmail.com>
2025-07-28 13:34:54 +0700

    open Admin routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import { Toaster } from 'sonner';
 
-// Import pages
+import { useUser } from './hooks/useUser.js';
+
+// Import pages (User)
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import BankAccountSetupPage from './pages/BankAccountSetupPage';
@@ -13,12 +15,17 @@ import Dashboard_activities from './pages/Dashboard/Dashboard_activities';
 import Dashboard_group from './pages/Dashboard/Dashboard_group';
 import Dashboard_statistics from './pages/Dashboard/Dashboard_statistics';
 
+// Import pages (Admin)
+import AdminDashboard from './pages/Admin/Admin_Dashboard';
+
 
 // Import components
 import OAuth2RedirectHandler from './components/googleOAuth/OAuth2RedirectHandler.jsx';
 import ProtectedLayout from './components/ProtectedLayout';
 
 function App() {
+  const { userData } = useUser();
+
   return (
       <div className="App">
         {/* ✅ Hiển thị thông báo toast */}
@@ -34,38 +41,69 @@ function App() {
           {/* OAuth2 redirect handler */}
           <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
 
-          {/* Protected routes */}
-          
-          <Route path="/dashboard/:id" element={
-            <ProtectedLayout> 
-              <Dashboard_main />
-            </ProtectedLayout>             
-            } 
-          />
-          <Route path="/dashboard/:id/activities" element={
-            <ProtectedLayout> 
-              <Dashboard_activities />
-            </ProtectedLayout>
-            }
-          />
-          <Route path="/dashboard/:id/group" element={
-            <ProtectedLayout> 
-              <Dashboard_group />
-            </ProtectedLayout>
-            }
-          />
-          <Route path="/dashboard/:id/statistics" element={
-            <ProtectedLayout>
-              <Dashboard_statistics />
-            </ProtectedLayout>
-            }
-          />
-          <Route path="/dashboard/:id/account" element={
-            <ProtectedLayout>
-              <AccountPage />
-            </ProtectedLayout>
-            }
-          />
+          {/* Protected routes với điều kiện role */}
+          {userData && userData.role === 'user' && (
+            <>
+              <Route
+                path="/dashboard/:id"
+                element={
+                  <ProtectedLayout>
+                    <Dashboard_main />
+                  </ProtectedLayout>
+                }
+              />
+
+              <Route
+                path="/dashboard/:id/activities"
+                element={
+                  <ProtectedLayout>
+                    <Dashboard_activities />
+                  </ProtectedLayout>
+                }
+              />
+
+              <Route
+                path="/dashboard/:id/group"
+                element={
+                  <ProtectedLayout>
+                    <Dashboard_group />
+                  </ProtectedLayout>
+                }
+              />
+
+              <Route
+                path="/dashboard/:id/statistics"
+                element={
+                  <ProtectedLayout>
+                    <Dashboard_statistics />
+                  </ProtectedLayout>
+                }
+              />
+              <Route
+                path="/dashboard/:id/account"
+                element={
+                  <ProtectedLayout>
+                    <AccountPage />
+                  </ProtectedLayout>
+                }
+              />
+            </>
+          )}
+
+          {userData && userData.role === 'admin' && (
+            <>
+              <Route
+                path="/admin/dashboard/:id"
+                element={
+                  <ProtectedLayout>
+                    <AdminDashboard />
+                  </ProtectedLayout>
+                } 
+              />
+              
+              {/* Thêm các route admin khác nếu cần */}
+            </>
+          )}
 
           
 
